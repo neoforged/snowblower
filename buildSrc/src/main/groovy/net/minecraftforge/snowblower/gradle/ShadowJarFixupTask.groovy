8f9397c7beb03434a394afc5ea454c6ac47c0314
SizableShrimp <sizableshrimp@sizableshrimp.me>
2023-02-02 13:28:23 -0600

    Fix gradle-wrapper.jar being included in shadowJar

---
+++ buildSrc/src/main/groovy/net/minecraftforge/snowblower/gradle/ShadowJarFixupTask.groovy
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) Forge Development LLC
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
+
+package net.minecraftforge.snowblower.gradle
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.artifacts.Configuration
+import org.gradle.api.file.RegularFileProperty
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.InputFile
+import org.gradle.api.tasks.OutputFile
+import org.gradle.api.tasks.TaskAction
+
+import java.nio.file.FileSystem
+import java.nio.file.FileSystems
+import java.nio.file.Files
+import java.nio.file.Path
+import java.security.MessageDigest
+
+abstract class ShadowJarFixupTask extends DefaultTask {
+    @InputFile
+    abstract RegularFileProperty getInput()
+
+    @OutputFile
+    abstract RegularFileProperty getOutput()
+
+    @TaskAction
+    void run() {
+        try (def zipFs = FileSystems.newFileSystem(input.get().asFile.toPath(), new HashMap<String, Object>())) {
+            def start = zipFs.getPath('/gradle/wrapper/gradle-wrapper.zip')
+            def end = zipFs.getPath('/gradle/wrapper/gradle-wrapper.jar')
+
+            if (Files.isRegularFile(start) && !Files.exists(end))
+                Files.move(start, end)
+        }
+    }
+}
